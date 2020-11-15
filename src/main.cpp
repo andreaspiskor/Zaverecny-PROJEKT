9e2bc2b05896f97922c49627fe1cd4b24981b07d
andreaspiskor <a.piskor007@email.cz>
2020-11-15 20:25:36 +0100

    1.4.2

--- src/main.cpp
+++ src/main.cpp
@@ -95,3 +95,19 @@ void loop() {
   }
 }
 */
+
+/*
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h> 
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
+void setup() {
+  lcd.init();
+  lcd.backlight();
+}
+void loop() {
+  lcd.setCursor(2, 0);
+  lcd.print("Nazdar!");
+  lcd.setCursor(2, 1);
+  lcd.print("LCD Display");
+}
+*/
